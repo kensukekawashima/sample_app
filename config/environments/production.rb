dacd6d2ef5682e9064319ef5693734cc4cd73782
kensukekawashima <kenken.14@icloud.com>
2020-10-26 11:54:28 +0900

    fixed production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://salty-meadow-46163.herokuapp.com/users'
+  host = 'https://salty-meadow-46163.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD']
-    :dimain => 'heroku.com',
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
 
