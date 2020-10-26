d94b3d71b42451bcd1059b1f59515a224e43e5eb
kensukekawashima <kenken.14@icloud.com>
2020-10-26 22:18:02 +0900

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 's1a2m3p4l5eapptutorial.herokuapp.com'
+  host = 'https://s1a2m3p4l5eapptutorial.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
