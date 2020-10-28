8f4dfef33693459e4b647d23024b7a7b00655939
kensukekawashima <kenken.14@icloud.com>
2020-10-28 18:57:18 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #followingの場合、DBではfollowedとなっているがfollowingだと自動的に探せないためsourceを用いて参照先を教える
+  has_many :following, through: :active_relationships, source: :followed
+  #followersと複数形だがrailsが自動的に単数形にし外部キーfollower_idを探してくれる
+  has_many :followers, through: :passive_relationships
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -67,7 +73,24 @@ class User < ApplicationRecord
   #試作feedの定義
   #完全な実装は次章の「ユーザーをフォローする」を参照
   def feed
-    microposts
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  #ユーザーをフォローする
+  def follow(other_user)
+    #<<演算子 (Shovel Operator) で配列の最後に追記することができる
+    following<<other_user
+  end
+
+  #ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user).destroy
+  end
+
+  #現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -22,10 +22,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as (@user)
     get root_path
     assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
     assert_select "a[href=?]", users_path
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", logout_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
 
   # test "the truth" do
